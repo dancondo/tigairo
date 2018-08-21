5d62192946e46216e6151eef6cc0630426e77bd4
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-21 20:57:49 -0300

    photo uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,6 +3,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   storage :fog                    # <- and this
 
+  version :avatar do
+    resize_to_fill 50, 50
+  end
+
   version :standard do
     resize_to_fill 400, 300
   end
