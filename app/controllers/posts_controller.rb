609d6514c6178024237798335dbe94bd0915b293
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-21 15:13:20 -0300

    Add dotenv file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+categories = %w[ごはん 友達との接し方 お店の人 ファッション お祝い 時間 街 仕事 恋愛 その他]
+categories.each do |category|
+  Category.create!(name: category)
+end
