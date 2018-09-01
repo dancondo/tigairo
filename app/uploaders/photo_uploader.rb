edc0e8844bdc73d5a7b6f0f7d600d57ceb43f39b
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-08-31 22:46:49 -0300

    quick fix

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -9,24 +9,33 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   version :thumb do
     process :crop
+    process :auto_orient
     resize_to_fill(400, 400)
   end
 
   version :large_thumb do
     process :crop
-    resize_to_fill(1200, 1200)
+    process :auto_orient
+    resize_to_fill(600, 600)
   end
 
   def crop
     if model.crop_x.present?
-      resize_to_limit(1200, 1200)
+      resize_to_limit(600, 600)
       manipulate! do |img|
-        x = model.crop_x.to_i * 2
-        y = model.crop_y.to_i * 2
-        w = model.crop_w.to_i * 2
-        h = model.crop_h.to_i * 2
+        x = model.crop_x.to_i
+        y = model.crop_y.to_i
+        w = model.crop_w.to_i
+        h = model.crop_h.to_i
         img.crop!(x, y, w, h)
       end
     end
   end
+
+  def auto_orient
+    manipulate! do |img|
+      img = img.auto_orient
+    end
+  end
+
 end
