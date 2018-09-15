766efb58a8b0b6cdce8ee739d0deb31b74fbfd79
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-09-15 16:56:01 -0300

    change comments to polymorphic to be able to comment itself, and create a method to count all the replies withing a post or a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
-  before_action :set_post, only: [:create, :update]
   before_action :set_user, only: [:create, :update]
+  before_action :set_comment_target, only: [:create, :update]
 
   def create
     @comment = Comment.new(comment_params)
     authorize @comment
     @comment.user = @user
-    @comment.post = @post
+    @comment.comment_target = @comment_target
     if @comment.save
       redirect_to posts_url
     else
@@ -16,16 +16,16 @@ class CommentsController < ApplicationController
 
   private
 
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
-
   def set_user
     @user = current_user
   end
 
   def comment_params
-    params.require(:comment).permit(:post_id, :user_id, :content)
+    params.require(:comment).permit(:comment_target_id, :comment_target_name, :user_id, :content)
   end
 
+  def set_comment_target
+    @comment_target = Post.find(params[:post_id]) if params[:post_id]
+    @comment_target = Comment.find(params[:comment_id]) if params[:comment_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   end
   resources :comments, only: :index do
     post 'reactions/change', to: 'reactions#change', as: 'reaction'
+    resources :comments, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
